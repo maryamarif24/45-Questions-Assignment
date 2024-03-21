02cc4c0dd6cff975cde9b2ea820e5dab7855ed6a
Maryam Arif <160311643+maryamarif24@users.noreply.github.com>
2024-03-21 15:33:56 +0500

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,2 @@
+var myName = "Maryam Arif";
+console.log('Hello', myName, ', would you like to learn some TypeScript today?');
---
+++ 1.ts
@@ -0,0 +1,3 @@
+let myName = "Maryam Arif";
+
+console.log('Hello',myName, ', would you like to learn some TypeScript today?')
