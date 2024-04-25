76f703dc56f5d750c4e835fd566c2eac996d87c3
Maryam Arif <160311643+maryamarif24@users.noreply.github.com>
2024-04-25 15:26:56 +0500

    Add files via upload

---
+++ 14.js
@@ -0,0 +1,4 @@
+var myGuests = ["Haya Suleiman", "Cihan Sikander", "Khadija Sikander"];
+myGuests.forEach(function (myGuests) {
+    console.log('Dear', myGuests, ', would you like to join me for dinner?');
+});
---
+++ 14.ts
@@ -0,0 +1,5 @@
+let myGuests : String[] = ["Haya Suleiman", "Cihan Sikander", "Khadija Sikander"];
+
+myGuests.forEach(myGuests => {
+    console.log('Dear', myGuests, ', would you like to join me for dinner?',)
+})
