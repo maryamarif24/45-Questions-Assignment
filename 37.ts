385828a77d7a9f00a2a3bc7013ab54a1664ce093
Maryam Arif <160311643+maryamarif24@users.noreply.github.com>
2024-06-04 22:05:17 +0500

    Day 13

---
+++ 37.ts
@@ -0,0 +1,7 @@
+function make_shirt(size: string = "large", message: string = "I love TypeScript") {
+    console.log("Making a", size, "T-Shirt with a message", message, "printed on it.");
+}
+
+make_shirt();
+make_shirt("medium");
+make_shirt("small", "Dive into coding");
---
+++ 39.js
@@ -0,0 +1,6 @@
+function city_country(city, country) {
+    return city, country;
+}
+console.log(city_country("Tokyo", "Japan"));
+console.log(city_country("Istanbul", "Turkey"));
+console.log(city_country("Karachi", "Pakistan"));
