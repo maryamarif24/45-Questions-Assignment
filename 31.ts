9a00df52c0afd24dac88a30b3e6707e45834ea60
Maryam Arif <160311643+maryamarif24@users.noreply.github.com>
2024-06-04 22:03:59 +0500

    Day 11

---
+++ 31.ts
@@ -0,0 +1,7 @@
+let usernames: string[] = [];
+
+if (usernames.length === 0) {
+    console.log("We need to find some users!");
+} else {
+    console.log("Welcome Users! You've successfully login this page.");
+}
